d5983ca9cdcc7b4d21bf1638ea49f8a7233b7587
RomanRti <roman.rtichshev@gmail.com>
2025-04-13 20:19:30 +0300

    fix: исправлен subject (валидный VAPID URL)

--- server/vapid-config.js
+++ server/vapid-config.js
@@ -2,5 +2,5 @@
 module.exports = {
   publicKey: "BPa9OZ4A9y7AXXXXXXX-EXAMPLE-KEYXXXXX",
   privateKey: "xF6Ya1ZXXXXX-EXAMPLE-PRIVATEKEYXXXX",
-  subject: "mailto:roman.rtichshev@gmail.com"
+  subject: "https://rss-pwa-server.onrender.com"
 };
