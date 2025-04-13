b1e30d68e974cae70fb2d15605cd92aad69b360d
RomanRti <roman.rtichshev@gmail.com>
2025-04-13 20:07:10 +0300

    Update vapid-config.js

--- server/vapid-config.js
+++ server/vapid-config.js
@@ -2,5 +2,5 @@
 module.exports = {
   publicKey: "BPa9OZ4A9y7AXXXXXXX-EXAMPLE-KEYXXXXX",
   privateKey: "xF6Ya1ZXXXXX-EXAMPLE-PRIVATEKEYXXXX",
-  subject: https://rss-pwa-server.onrender.com/
+  subject: "https://rss-pwa-server.onrender.com/"
 };
