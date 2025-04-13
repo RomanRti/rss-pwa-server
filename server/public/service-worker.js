a5784df4d3322aeb691190fa93b9e5fa03ca5ecd
RomanRti <roman.rtichshev@gmail.com>
2025-04-13 15:14:10 +0300

    Add files via upload

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -1,1 +1,15 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());self.addEventListener('push',e=>{const{title:t,body:s,url:n}=e.data.json(),i={body:s,icon:'/icon-192.png',data:{url:n}};e.waitUntil(self.registration.showNotification(t,i))});self.addEventListener('notificationclick',e=>{e.notification.close(),e.waitUntil(clients.openWindow(e.notification.data.url))});
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('push', event => {
+  const { title, body, url } = event.data.json();
+  const options = {
+    body,
+    icon: '/icon-192.png',
+    data: { url }
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url));
+});
