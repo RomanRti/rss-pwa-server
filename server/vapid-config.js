0445a34a8f9834034b97ef0bcc6b2a7224a9d37e
RomanRti <roman.rtichshev@gmail.com>
2025-04-13 19:21:42 +0300

    Add files via upload

---
+++ server/vapid-config.js
@@ -0,0 +1,6 @@
+// VAPID configuration
+module.exports = {
+  publicKey: "BPa9OZ4A9y7AXXXXXXX-EXAMPLE-KEYXXXXX",
+  privateKey: "xF6Ya1ZXXXXX-EXAMPLE-PRIVATEKEYXXXX",
+  subject: "mailto:you@example.com"
+};
